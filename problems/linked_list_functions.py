b26b26d8b37f986cb8e3cb7bf8ced9633da2f20f
mhendricks96 <mhendricks96@yahoo.com>
2021-12-09 14:51:54 -0800

    working on linked list functions. still on the one to find the middle

--- problems/linked_list_functions.py
+++ problems/linked_list_functions.py
@@ -1,12 +1,7 @@
+from data_structures.singly_linked_list import Node, SinglyLinkedList
+
 # given a linked list, return a list of all duplicate values
 
-#define a function that takes in a linked list
-#define an empty list to hold answer
-# set up an empty dictionary to hold the values and their counts
-# set up a while loop that goes until a node is none
-# each node, check to see if its value is in the dictionary as a key. if so, add 1 to its value. if not add it with a value of 1
-#check every key in the dictionary. if its value is more than 1, add it to the list
-# return the list
 
 def return_duplicates(linked):
   answer = []
@@ -16,7 +11,8 @@ def return_duplicates(linked):
   while current is not None:
     if current.value in values_dict:
       values_dict[current.value] += 1
-    else values_dict[current.value] = 1
+    else:
+      values_dict[current.value] = 1
     current = current.next
   
   for item in values_dict:
@@ -24,3 +20,76 @@ def return_duplicates(linked):
       answer.append(item)
   
   return answer
+
+
+#given a linked list, find the maximum value recursively
+
+def find_max_recursively(linked):
+  
+  def recursive_part(node, max_value):
+    
+    if node.value > max_value:
+      max_value = node.value
+    
+    if node.next is None:
+      return max_value
+    else:
+      return recursive_part(node.next, max_value)
+
+  max_value = recursive_part(linked.head, linked.head.value)
+  
+  return max_value
+
+#Merge two sorted linked lists from their end
+
+#For example, consider lists a = {1, 3, 5} and b = {2, 6, 7, 10}. Merging both lists should yield the list {10, 7, 6, 5, 3, 2, 1}.
+
+
+def merge_opposite(ll1, ll2):
+  new_ll = SinglyLinkedList()
+
+  ll1_pointer = ll1.head
+  ll2_pointer = ll2.head
+  
+  while ll1_pointer and ll2_pointer:
+    
+    if ll1_pointer.value < ll2_pointer.value:
+      new_ll.insert(ll1_pointer.value)
+      ll1_pointer = ll1_pointer.next
+
+    elif ll2_pointer.value < ll1_pointer.value:
+      new_ll.insert(ll2_pointer.value)
+      ll2_pointer = ll2_pointer.next
+
+  while ll1_pointer:
+    new_ll.insert(ll1_pointer.value)
+    ll1_pointer = ll1_pointer.next
+
+  while ll2_pointer:
+    new_ll.insert(ll2_pointer.value)
+    ll2_pointer = ll2_pointer.next
+  
+  return new_ll
+
+# find the middle of a linked list using 2 pointers (fast and slow)
+
+# define a function that takes in a linked list
+# create a variable for a slow pointer
+# create a variable for fast pointer
+# create a while loop that goes until the fast pointer ends
+# every iteration the fast pointer moves 2 nodes and the slow pointer moves 1 node
+# return the value of the node the first pointer is at
+
+def find_middle(linked):
+  slow_pointer = linked.head
+  fast_pointer = linked.head
+
+  while fast_pointer:
+    fast_pointer = fast_pointer.next.next
+    slow_pointer = slow_pointer.next
+  
+  return slow_pointer.value
+
+  
+
+  
--- test_linked_list.py
+++ test_linked_list.py
@@ -1,4 +1,5 @@
 from data_structures.singly_linked_list import Node, SinglyLinkedList
+from problems.linked_list_functions import return_duplicates, find_max_recursively, merge_opposite, find_middle
 
 def test_create_node():
   my_node = Node(99)
@@ -27,3 +28,54 @@ def test_create_and_add():
   actual = str(my_list)
   expected = "{3} -> {2} -> {1} -> NONE"
   assert actual == expected
+
+def test_return_duplicate():
+  my_list = SinglyLinkedList()
+  my_list.insert(1)
+  my_list.insert(2)
+  my_list.insert(3)
+  my_list.insert(4)
+  my_list.insert(2)
+  my_list.insert(3)
+  actual = return_duplicates(my_list)
+  expected = [3,2]
+  assert actual == expected
+
+def test_recursive_max():
+  my_list = SinglyLinkedList()
+  my_list.insert(1)
+  my_list.insert(5)
+  my_list.insert(9)
+  my_list.insert(77)
+  my_list.insert(12)
+  my_list.insert(3)
+  actual = find_max_recursively(my_list)
+  expected = 77
+  assert actual == expected
+
+def test_merge_opposite():
+  my_list1 = SinglyLinkedList()
+  my_list1.insert(5)
+  my_list1.insert(3)
+  my_list1.insert(1)
+  my_list2 = SinglyLinkedList()
+  my_list2.insert(10)
+  my_list2.insert(7)
+  my_list2.insert(6)
+  my_list2.insert(2)
+  actual = str(merge_opposite(my_list1, my_list2))
+  expected = "{10} -> {7} -> {6} -> {5} -> {3} -> {2} -> {1} -> NONE"
+  assert actual == expected
+
+def test_find_middle():
+  my_list1 = SinglyLinkedList()
+  my_list1.insert(5)
+  my_list1.insert(3)
+  my_list1.insert(1)
+  my_list1.insert(10)
+  my_list1.insert(7)
+  my_list1.insert(6)
+  my_list1.insert(2)
+  actual = find_middle(my_list1)
+  expected = 10
+  #assert actual == expected
