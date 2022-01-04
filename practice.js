0c7eee3d80d0f6cc88c2b5e8f8f6fa090f805e67
mhendricks96 <mhendricks96@yahoo.com>
2022-01-04 14:52:57 -0800

    practicing js

---
+++ practice.js
@@ -0,0 +1,47 @@
+// just wanna use this to get back into the swing of javascript after a year of python. going to write some basic functons for
+
+// function to add 2 numbers
+
+function add_numbers(x, y) {
+  return x + y;
+}
+
+// find the maximum value in an array
+
+function find_maximum(arr) {
+  var maximum = 0;
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] > maximum) {
+      maximum = arr[i];
+    }
+  }
+  return maximum;
+}
+
+// or
+
+function max(arr) {
+  var maximum = math.max(arr);
+  return maximum
+}
+
+// return every item in an array that appears 3 times or more
+
+function find_triples(arr) {
+  var value_counts = new Map();
+  var final_array = [];
+  for (var i = 0; i < arr.length; i++) {
+    if (value_counts.has(arr[i])) {
+      value_counts.set(arr[i], value_counts.get(arr[i]) + 1);
+    }
+    else {
+      value_counts.set(arr[i], 0)
+    }
+  }
+  for (var [key, value] of value_counts) {
+    if (value > 2){
+      final_array.push(value);
+    }
+  }
+  return final_array
+}
